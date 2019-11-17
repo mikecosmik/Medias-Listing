52ec51b97a9897056d2adc5c364efcd0c42650fd
Mike <mikecosmik@gmail.com>
2019-11-16 22:35:34 -0500

    Update du dump

--- medias.sql
+++ medias.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 16 nov. 2019 à 22:45
+-- Généré le :  Dim 17 nov. 2019 à 03:34
 -- Version du serveur :  5.7.21
 -- Version de PHP :  5.6.35
 
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `media_element` (
   `fk_format` int(11) DEFAULT NULL,
   `fk_type` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `media_element`
@@ -73,7 +73,16 @@ CREATE TABLE IF NOT EXISTS `media_element` (
 
 INSERT INTO `media_element` (`id`, `titre`, `description`, `note`, `fk_format`, `fk_type`) VALUES
 (1, 'NHL 16', 'Meilleur jeu de hockey ever!  Mais désuet... note à moi-même acheter le NHL 20 au PC!!! :)', 5, 5, 2),
-(2, 'Mario Kart 7 3DS', 'Un classique de course Nintendo', 5, 6, 3);
+(2, 'Mario Kart 7 3DS', 'Un classique de course Nintendo', 5, 6, 3),
+(3, 'Titre', 'description', 3, 2, 6),
+(4, 'Titre 2', 'Desc 2', 1, 1, 1),
+(5, 'Super Mario 3D Land', 'Un classique Nintendo', 4, 6, 3),
+(6, 'Star Wars Battlefront Ultimate Edition', 'Combat en ligne / solo', 4, 5, 2),
+(7, 'The Amazing Spiderman 2', '-', 3, 5, 2),
+(8, 'Resident Evil 6', '-', 5, 5, 2),
+(9, 'Injustice 2', 'Combat 1vs1', 34, 5, 2),
+(10, 'Need For Speed Rivals', 'Course', 4, 5, 2),
+(11, 'Titanfall 2', 'Guerre robots', 4, 5, 2);
 
 -- --------------------------------------------------------
 
