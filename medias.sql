ed460e21cff6c43982131993f7cc546d833e23a0
mikecosmik <mikecosmik@gmail.com>
2019-11-17 10:02:03 -0500

    sql dump

--- medias.sql
+++ medias.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 17 nov. 2019 à 03:34
--- Version du serveur :  5.7.21
--- Version de PHP :  5.6.35
+-- Hôte : 127.0.0.1
+-- Généré le :  Dim 17 nov. 2019 à 16:01
+-- Version du serveur :  10.4.8-MariaDB
+-- Version de PHP :  7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,12 +28,10 @@ SET time_zone = "+00:00";
 -- Structure de la table `format`
 --
 
-DROP TABLE IF EXISTS `format`;
-CREATE TABLE IF NOT EXISTS `format` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+CREATE TABLE `format` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `format`
@@ -56,16 +54,14 @@ INSERT INTO `format` (`id`, `nom`) VALUES
 -- Structure de la table `media_element`
 --
 
-DROP TABLE IF EXISTS `media_element`;
-CREATE TABLE IF NOT EXISTS `media_element` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `media_element` (
+  `id` int(11) NOT NULL,
   `titre` varchar(255) DEFAULT NULL,
-  `description` text,
-  `note` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `note` float DEFAULT NULL,
   `fk_format` int(11) DEFAULT NULL,
-  `fk_type` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+  `fk_type` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `media_element`
@@ -74,13 +70,11 @@ CREATE TABLE IF NOT EXISTS `media_element` (
 INSERT INTO `media_element` (`id`, `titre`, `description`, `note`, `fk_format`, `fk_type`) VALUES
 (1, 'NHL 16', 'Meilleur jeu de hockey ever!  Mais désuet... note à moi-même acheter le NHL 20 au PC!!! :)', 5, 5, 2),
 (2, 'Mario Kart 7 3DS', 'Un classique de course Nintendo', 5, 6, 3),
-(3, 'Titre', 'description', 3, 2, 6),
-(4, 'Titre 2', 'Desc 2', 1, 1, 1),
 (5, 'Super Mario 3D Land', 'Un classique Nintendo', 4, 6, 3),
 (6, 'Star Wars Battlefront Ultimate Edition', 'Combat en ligne / solo', 4, 5, 2),
-(7, 'The Amazing Spiderman 2', '-', 3, 5, 2),
-(8, 'Resident Evil 6', '-', 5, 5, 2),
-(9, 'Injustice 2', 'Combat 1vs1', 34, 5, 2),
+(7, 'The Amazing Spiderman 2', 'Semblable à Watchdogs', 3, 5, 2),
+(8, 'Resident Evil 6', 'Zombies...', 5, 5, 2),
+(9, 'Injustice 2', 'Combat 1 contre 1', 4, 5, 2),
 (10, 'Need For Speed Rivals', 'Course', 4, 5, 2),
 (11, 'Titanfall 2', 'Guerre robots', 4, 5, 2);
 
@@ -90,12 +84,10 @@ INSERT INTO `media_element` (`id`, `titre`, `description`, `note`, `fk_format`, 
 -- Structure de la table `type`
 --
 
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE IF NOT EXISTS `type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `type`
@@ -110,6 +102,50 @@ INSERT INTO `type` (`id`, `nom`) VALUES
 (6, 'Wii'),
 (7, 'Film'),
 (8, 'Autre');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `format`
+--
+ALTER TABLE `format`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `media_element`
+--
+ALTER TABLE `media_element`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `format`
+--
+ALTER TABLE `format`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `media_element`
+--
+ALTER TABLE `media_element`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT pour la table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
